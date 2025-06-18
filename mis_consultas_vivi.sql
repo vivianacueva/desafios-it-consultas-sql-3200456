0f4dee2d17e54d88e17e7e86f2275526b02bcfb6
Viviana <viviana.cueva85@outlook.com>
2025-06-18 18:45:02 +0200

    consultas4y5

--- mis_consultas_vivi.sql
+++ mis_consultas_vivi.sql
@@ -12,3 +12,22 @@ select distinct p.id, p.nombre
 from pelicula p
 left join cartelera c on p.id=c.id_pelicula
 where p.clasificacion_edad = 0 and c.id_pelicula is not null;
+
+-- 🔍 Consulta 04: Filtrar cines que estén disponibles
+-- ----------------------------------------
+select distinct c.* 
+from cine c
+inner join cartelera on c.id = cartelera.id_cine;
+
+-- 🔍 Consulta 05: Películas en cada cine 
+-- ----------------------------------------
+select 
+p.nombre as pelicula, 
+c.nombre as cine,
+c.estado as estado,
+c.pais as pais
+from pelicula p 
+right join cartelera ca on ca.id_pelicula = p.id
+right join cine c on c.id = ca.id_cine
+where p.nombre is not null;
+
